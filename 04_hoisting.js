a1883eabfc0dd78e25e716a148fa178be4e43e68
IRONKAGE <IRONKAGE@gmail.com>
2020-03-16 20:13:58 +0200

    add Hoisting

---
+++ 04_hoisting.js
@@ -0,0 +1,44 @@
+// function sum (a, b) {
+//     return a + b
+// }
+// console.log(sum(1, 3))
+
+// console.log(sum(1, 3))
+// function sum (a, b) {
+//     return a + b
+// }
+
+
+
+// console.log(i);
+// var i = 42
+// console.log(i)
+
+// var i
+// console.log(i);
+// i = 42
+// console.log(i)
+
+// console.log(num);
+// const num = 42
+// console.log(num);
+
+// console.log(num);
+// let num = 42
+// console.log(num);
+
+
+
+
+
+// Function Expression & Function Declaration
+console.log(D_square(25));
+function D_square(number) { //Function Declaration
+    return number ** 2
+}
+
+
+var E_square = function(num) { //Function Expression
+    return num ** 2
+}
+console.log(E_square(25));
