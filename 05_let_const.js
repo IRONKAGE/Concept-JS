60b73cb7b24456a4e22c86a5e9fca9851922b8b0
IRONKAGE <IRONKAGE@gmail.com>
2020-03-16 20:14:39 +0200

    add Let & Const

---
+++ 05_let_const.js
@@ -0,0 +1,42 @@
+// Let
+// let a = 'Variable a'
+// let b = 'Variable b'
+
+// {
+//     a = 'New Varieble A'
+//     let b = 'Local Variable B'
+//     console.log('Scope A', a);
+//     console.log('Scope B', b);
+//     // console.log('Scope C', c);
+//     // let c = 'Something'
+// }
+
+// console.log('A', a);
+// console.log('B', b);
+
+
+
+
+
+// Const
+// const PORT = 8080
+// PORT = 2000
+
+
+// const array = ['JS', 'is', 'NotBad']
+// array.push('!')
+// array[0] = 'Python'
+// console.log(array)
+// array = ''
+
+
+const obj = {}
+obj.name = 'Oleh'
+obj.age = 26
+console.log(obj);
+
+obj.age = 21
+console.log(obj);
+
+delete obj.name
+console.log(obj);
